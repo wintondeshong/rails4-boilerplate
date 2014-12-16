e7426a7b9b84326f66c9dadbbb6a4d70cfc093a7
Winton DeShong <contact@wintondeshong.com>
2014-12-15 21:50:33 -0500

    Disable 404 redirection in development mode

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  #unless Rails.env.development?
+  unless Rails.env.development?
     match '*not_found', via: ActionDispatch::Routing::HTTP_METHODS, to: 'errors#error_404'
-  #end
+  end
 end
