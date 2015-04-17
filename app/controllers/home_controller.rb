c4577e949e2016581fc41d2687bea070c455d895
Winton DeShong <contact@wintondeshong.com>
2015-04-16 23:10:59 -0400

    Added dynamic robots.txt handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ApplicationController
   def index
   end
 
+  # provides environment specific
+  def robots
+    config_type = Rails.env.production? ? "production" : "development"
+    robots      = File.read(Rails.root + "config/robots.#{config_type}.txt")
+    render text: robots, layout: false, content_type: "text/plain"
+  end
+
 end
